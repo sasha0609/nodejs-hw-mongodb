4d627543a522af4576fc6c18a2fda60a15d7a345
sasha0609 <oleksandralavr@gmail.com>
2024-08-05 10:03:49 +0200

    update package.json

--- src/server.js
+++ src/server.js
@@ -2,12 +2,11 @@ import dotenv from 'dotenv';
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { env } from './env.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 
-const PORT = Number(env('PORT', '3000'));
+const PORT = Number(process.env.PORT) || 3000;
 
 export const setupServer = () => {
   const app = express();
