585716057d7df19f2dc4d0aa80e7c6364fdf8bb3
sasha0609 <oleksandralavr@gmail.com>
2024-08-12 16:57:44 +0200

    main update

---
+++ src/routers/contacts.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import {
+  createContactController,
+  deleteContactontroller,
+  getContactByIdController,
+  getContactsController,
+  homeController,
+  patchContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+router.get('/', ctrlWrapper(homeController));
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.patch('/contacts/:contactId', patchContactController);
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactontroller));
+
+export default router;
